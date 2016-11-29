913cdf7a84d43fdea85e096112f40c6202e867aa
dadi11 <dadi.sikorski@gmail.com>
2016-11-29 15:37:22 +0100

    Xenonify
    Change-Id: I4c735c63b750c7acc19279a4b272eb6cf06a29f0
    
    kenzo: add SuperSU apk
    
    Change-Id: I80a136ca15c5902231affc57c67b0570dbf8dbde
    
    kenzo: add ro.xenonhd.maintainer

---
+++ xenonhd.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
+
+# Inherit some common XenonHD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := kenzo
+PRODUCT_NAME := xenonhd_kenzo
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 3 Pro
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+WITH_SUPERSU := true
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=dadi11
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/6.11.24:user/release-keys \
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M 6.11.24 release-keys"
+endif
